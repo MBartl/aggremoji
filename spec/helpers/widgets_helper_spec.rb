8ce76f88b9a9d2f698d4c74e191c1e6f816f5051
Matt Bartl <mattbartl@gmail.com>
2020-06-15 15:00:04 -0500

    test

--- spec/helpers/widgets_helper_spec.rb
+++ spec/helpers/widgets_helper_spec.rb
@@ -10,6 +10,25 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe WidgetsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+describe WidgetsHelper do
+
+  describe ".add" do
+    context "two numbers" do
+      context "given '2,4'" do
+        it "returns 6" do
+          expect(StringCalculator.add("2,4")).to eql(6)
+        end
+      end
+
+      context "given '17,100'" do
+        it "returns 117" do
+          expect(StringCalculator.add("17,100")).to eql(117)
+        end
+      end
+    end
+  end
 end
+# RSpec.describe WidgetsHelper, type: :helper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
