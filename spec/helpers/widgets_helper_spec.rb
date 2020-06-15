0c412ce2b369a500c827642f1248b9073a305bf2
Matt Bartl <mattbartl@gmail.com>
2020-06-15 15:10:11 -0500

    testing framework complete - all green on 15 sample tests

--- spec/helpers/widgets_helper_spec.rb
+++ spec/helpers/widgets_helper_spec.rb
@@ -17,13 +17,13 @@ describe WidgetsHelper do
     context "two numbers" do
       context "given '2,4'" do
         it "returns 6" do
-          expect(StringCalculator.add("2,4")).to eql(6)
+          expect(WidgetsHelper.add("2,4")).to eql(6)
         end
       end
 
       context "given '17,100'" do
         it "returns 117" do
-          expect(StringCalculator.add("17,100")).to eql(117)
+          expect(WidgetsHelper.add("17,100")).to eql(117)
         end
       end
     end
